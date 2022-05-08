f421571008dca17f8ca6713f48db95c9bb8d6e56
AJIT SHARMA <ajit08082000@gmail.com>
2022-05-08 16:45:07 +0530

    fixed the loading issues

--- src/components/bmi/bmi.js
+++ src/components/bmi/bmi.js
@@ -10,32 +10,33 @@ const BodyMassIndex = () => {
   const [heightConvertor, setHeightConvertor] = useState(1);
   const [weightConvertor, setWeightConvertor] = useState(1);
   const calcBmi = () => {
-    let bmi = (weight / (height * height*weightConvertor) * heightConvertor);
+    let bmi = (weight / (height * height * weightConvertor)) * heightConvertor;
     if (weight === 0 || weight === "" || height === 0 || height === "") {
       alert("Enter valid weight and height !");
     } else {
       setBmi(bmi.toFixed(2));
     }
-if(bmi<16 || bmi===0){
-    setMsg("Excessive Underweight, You Need medical therapy !")
-    setColor("red");
-}
-   else if(bmi>16 && bmi<=18.5){
-        setMsg("You Are Underweight, oops !");
-    }else if(bmi>18.5 && bmi<=25){
-        setMsg("Your BMI is Normal, Healthy !")
-        setColor("green");
-    }else if(bmi>25 && bmi<40){
-        setMsg("You are Overweight, oops !")
-    }else{
-        setMsg("Excessive Overweight, You Need medical therapy !");
-        setColor("red");
+    if (bmi < 16 || bmi === 0) {
+      setMsg("Excessive Underweight, You Need medical therapy !");
+      setColor("red");
+    } else if (bmi > 16 && bmi <= 18.5) {
+      setMsg("You Are Underweight, oops !");
+    } else if (bmi > 18.5 && bmi <= 25) {
+      setMsg("Your BMI is Normal, Healthy !");
+      setColor("green");
+    } else if (bmi > 25 && bmi < 40) {
+      setMsg("You are Overweight, oops !");
+    } else {
+      setMsg("Excessive Overweight, You Need medical therapy !");
+      setColor("red");
     }
   };
-  const reload =()=>{
-      window.location.reload();
-     
-  }
+  const reload = () => {
+    if (weight.length > 0 || height.length > 0) {
+      setWeight(weight === 0);
+      setHeight(height === 0);
+    }
+  };
   return (
     <div className="bg-theme-changer bmi_body">
       <div className="pt-2">
@@ -43,7 +44,11 @@ if(bmi<16 || bmi===0){
         <div className="flex-col bmi_input_container pt-1">
           <div className="flex-wrap selection-unit">
             <h3 className="bmi_headings"> Enter Your Weight </h3>
-            <select className="select-property" value={weightConvertor} onChange={e=>setWeightConvertor(e.target.value)}>
+            <select
+              className="select-property"
+              value={weightConvertor}
+              onChange={(e) => setWeightConvertor(e.target.value)}
+            >
               <option value="1">kilograms</option>
               <option value="2.205">Pounds</option>
             </select>
@@ -56,7 +61,11 @@ if(bmi<16 || bmi===0){
           />
           <div className="flex-wrap selection-unit">
             <h3 className="bmi_headings"> Enter Your Height </h3>{" "}
-            <select className="select-property" value={heightConvertor} onChange={(e)=>setHeightConvertor(e.target.value)}>
+            <select
+              className="select-property"
+              value={heightConvertor}
+              onChange={(e) => setHeightConvertor(e.target.value)}
+            >
               <option value="1">Meters</option>
               <option value="10000">Centimeters </option>
 
@@ -80,7 +89,13 @@ if(bmi<16 || bmi===0){
         {weight.length > 0 && height.length > 0 > 0 ? (
           <div>
             <h3 className="flex-center mt-1">Bmi is || {bmi} </h3>
-            <h3 className="flex-center mt-1" style={{color:color,textAlign:"center"}}> {msg} </h3>
+            <h3
+              className="flex-center mt-1"
+              style={{ color: color, textAlign: "center" }}
+            >
+              {" "}
+              {msg}{" "}
+            </h3>
           </div>
         ) : (
           <h3 className="flex-center"> All inputs are Mandatory to Fill </h3>
@@ -88,7 +103,6 @@ if(bmi<16 || bmi===0){
       </div>{" "}
     </div>
   );
-  
 };
 
 export { BodyMassIndex };
