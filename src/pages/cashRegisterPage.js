a44eaf5910fc908531ec303ee8f3030c395c41ad
AJIT SHARMA <ajit08082000@gmail.com>
2022-05-15 18:22:47 +0530

    - added cash-manager to mini-project

---
+++ src/pages/cashRegisterPage.js
@@ -0,0 +1,12 @@
+import {Navbar} from "../components/Navbar/Navbar"
+import { CashRegister } from "../components/cashRegister/crm";
+
+const CashRegisterPage = () => {
+  return (
+    <div>
+        <Navbar />
+      <CashRegister />
+    </div>
+  );
+};
+export { CashRegisterPage };
